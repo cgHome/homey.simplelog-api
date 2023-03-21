591205829696a09af3893c6a668708ff3d050829
cgHome <cghome@cflat-inc.org>
2023-03-21 15:06:31 +0100

    first commit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 'use strict';
 
-module.exports.SimpleLogAPI = require('./lib/simpleLogAPI');
+// module.exports.SimpleLogAPI = require('./lib/simpleLogAPI');
 module.exports.SimpleLogMixin = require('./lib/simpleLogMixin');
